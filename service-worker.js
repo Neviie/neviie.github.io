4623de32765e72ed350951e623d1219affb62739
Neviie <105316974+Neviie@users.noreply.github.com>
2023-05-27 23:25:46 +0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/site2.html',
+        '/offline.js',
+        '/icon.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
