6149569850a7d9338c29ecc444df587bd7d3d280
Neviie <105316974+Neviie@users.noreply.github.com>
2023-08-17 00:37:44 +0500

    Update 2_script.js

--- Scripts/2_script.js
+++ Scripts/2_script.js
@@ -25,3 +25,24 @@
 		/* выводим текущую дату и время на сайт в блок с id "current_date_time_block" */
 		document.getElementById('current_date_time_blockk').innerHTML = date_time();
 		document.getElementById('date_date').innerHTML = date_time();
+
+		// Убирает элемент ost и меняет его класс чтобы он не изменялся
+		var ost = document.querySelector('.ost');
+			ost.style.display = "none";
+
+			ost.classList.remove("ost");
+			ost.classList.add("ostt");
+
+		// Меняет элементы qr и new_control местами
+		var element1 = document.querySelector(".new_control");
+		var element2 = document.querySelector(".qr");
+		var parent = element1.parentNode;
+			
+			var temp = document.createElement("div");
+			parent.insertBefore(temp, element1);
+			
+			parent.insertBefore(element1, element2);
+			parent.insertBefore(element2, temp);
+			
+			parent.removeChild(temp);
+		
