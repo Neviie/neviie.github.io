edb62859d6c0e12820e53bd88eb8618afb879bf3
Neviie <105316974+Neviie@users.noreply.github.com>
2023-09-22 03:30:13 +0500

    Add files via upload

--- Scripts/4_script.js
+++ Scripts/4_script.js
@@ -1,59 +1,71 @@
+window.onload= reddirect;
+
+const container = document.querySelector('.container');
+const fon = document.querySelector('.fon');
+var ups = document.querySelector(".ups");
+
 function reddirect() {
     var savedValue = localStorage.getItem("myValue");
     if (savedValue) {
+        
         document.getElementById("inputValue").value = savedValue;
-
         var value = localStorage.getItem("myValue");
         userId = value; // Присваиваем значение переменной userId
         const fakeParam = Date.now();
-        const themeurl = 'https://api.puzzlebot.top/api?token=2s1OVLz5iHnPeU7dp8ZGAUrFww8cQ4p9&method=getVariableValue&variable=custom_variable_TXYR7BA0XLI7SQY0&user_id=';
-        const proxyUrl = 'http://31.129.33.76:8080/';
-        const themefullUrl = (themeurl + userId);
-        const numberurl = 'https://api.puzzlebot.top/api?token=2s1OVLz5iHnPeU7dp8ZGAUrFww8cQ4p9&method=getVariableValue&variable=number&user_id=';
-        const numberfullUrl = (numberurl + userId);
-        var ups = document.querySelector(".ups");
-        let firstFetchValue = 0;
-        let secondFetchValue = 0;
-        
-        var fetch1 = fetch(proxyUrl + themefullUrl + '?fakeParam=' + fakeParam)
-        .then(response => response.json())
-        .then(data => {
-            firstFetchValue = data.data;
-            handleFetchResponse(data.data);
-            console.log('Первый fetch:', data.data);
-        })
-        .catch(error => console.error(error));
+        const index = 'https://api.puzzlebot.top/api?token=2s1OVLz5iHnPeU7dp8ZGAUrFww8cQ4p9&method=getVariableValue&variable=index&user_id=';
+        const proxyUrl = 'https://avt59.ru.:8443/';
+        const indexfull = (index + userId);
+        let theme = 0;
+        let BusNumber = 0;
 
-        var fetch2 = fetch(proxyUrl + numberfullUrl + '?fakeParam=' + fakeParam)
-        .then(response => response.json())
-        .then(data => {
-            secondFetchValue = data.data;
-            handleFetchResponse(data.data);
-            console.log('Второй fetch:', data.data);
-        })
-        .catch(error => console.error(error));
-        
-        function handleFetchResponse(data) {
-            let numericValue = parseInt(secondFetchValue.startsWith("TRA") || secondFetchValue.startsWith("TWN") ? secondFetchValue.substring(3) : secondFetchValue);
-
-            if (firstFetchValue == 0 && numericValue >= 1 && numericValue <= 100) {
-                window.location.href = `https://neviie.github.io/WEB/Light/${secondFetchValue}.html`;
-            } else if (firstFetchValue == 1 && numericValue >= 1 && numericValue <= 100) {
-                window.location.href = `https://neviie.github.io/WEB/Night/N${secondFetchValue}.html`;
-            } else {
+        fetch(proxyUrl + indexfull + '?fakeParam=' + fakeParam)
+        .then(response => {
+            if (!response.ok) {
                 ups.style.display = 'block';
             }
-        // Другие условия, если необходимо
-    }} 
-    
+            return response.json();
+        })
+        .then(data => {
+            if (data && data.data) {
+                const variables = data.data.split(' ; ');
+                theme = variables[0];
+                BusNumber = variables[1];
+
+                handleFetchResponse(data.data);
+                console.log("theme: " + theme + ", BusNumber: " + BusNumber);
+            } else {
+
+                container.classList.replace('hidden', 'block');
+                fon.classList.replace('hidden', 'block');
+
+                savedValue = "Неверный ID";
+                document.getElementById("inputValue").value = savedValue;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            document.getElementById("inputValue").value = "неверный id";
+        });
+
+    function handleFetchResponse(data) {
+        let numericValue = parseInt(BusNumber.startsWith("TRA") || BusNumber.startsWith("TWN") ? BusNumber.substring(3) : BusNumber);
+
+        if (theme == 0 && numericValue >= 1 && numericValue <= 100) {
+            window.location.href = `/WEB/Light/${BusNumber}.html`;
+        } else if (theme == 1 && numericValue >= 1 && numericValue <= 100) {
+            window.location.href = `/WEB/Night/N${BusNumber}.html`;
+        } else {
+            ups.style.display = 'block';
+        }
+    }
+    }
     else {
         // Если значение не сохранено в Local Storage, скрываем элементы
-        const container = document.querySelector('.container');
-        const fon = document.querySelector('.fon');
 
         container.classList.replace('hidden', 'block');
         fon.classList.replace('hidden', 'block');
     }
+
     };
  
    function saveData() {
@@ -61,14 +73,7 @@ function reddirect() {
      localStorage.setItem("myValue", value);
    }
    
-   function hideContainer() {
-    const container = document.querySelector('.container');
-    const fon = document.querySelector('.fon');
-    
+   function hideContainer() {   
     container.classList.replace('block', 'hidden');
     fon.classList.replace('block', 'hidden');
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    reddirect();
-});
